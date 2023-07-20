d14d814799c75744d5ed6575d67234d1eae418bc
PotnuruDeekshitha <139770330+PotnuruDeekshitha@users.noreply.github.com>
2023-07-20 18:46:07 +0530

    Add files via upload

---
+++ deekshitha iit class.py
@@ -0,0 +1,37 @@
+Python 3.7.7 (tags/v3.7.7:d7c567b08f, Mar 10 2020, 10:41:24) [MSC v.1900 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python37/deekshitha iit class.py
+Traceback (most recent call last):
+  File "C:/Users/user/AppData/Local/Programs/Python/Python37/deekshitha iit class.py", line 6, in <module>
+    det_A = np.linalg.det[A]
+TypeError: 'function' object is not subscriptable
+>>> 
+= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python37/deekshitha iit class.py
+Traceback (most recent call last):
+  File "C:/Users/user/AppData/Local/Programs/Python/Python37/deekshitha iit class.py", line 6, in <module>
+    det_A = np.linalg.det[A]
+TypeError: 'function' object is not subscriptable
+>>> 
+= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python37/deekshitha iit class.py
+Traceback (most recent call last):
+  File "C:/Users/user/AppData/Local/Programs/Python/Python37/deekshitha iit class.py", line 6, in <module>
+    det_A = np.linalg.det(A)
+  File "<__array_function__ internals>", line 6, in det
+  File "C:\Users\user\AppData\Local\Programs\Python\Python37\lib\site-packages\numpy\linalg\linalg.py", line 2155, in det
+    _assert_stacked_square(a)
+  File "C:\Users\user\AppData\Local\Programs\Python\Python37\lib\site-packages\numpy\linalg\linalg.py", line 203, in _assert_stacked_square
+    raise LinAlgError('Last 2 dimensions of the array must be square')
+numpy.linalg.LinAlgError: Last 2 dimensions of the array must be square
+>>> 
+= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python37/deekshitha iit class.py
+Traceback (most recent call last):
+  File "C:/Users/user/AppData/Local/Programs/Python/Python37/deekshitha iit class.py", line 12, in <module>
+    if(np.linaly.det(x_num)==0 and np.linaly.det(y_num)==0):
+  File "C:\Users\user\AppData\Local\Programs\Python\Python37\lib\site-packages\numpy\__init__.py", line 314, in __getattr__
+    "{!r}".format(__name__, attr))
+AttributeError: module 'numpy' has no attribute 'linaly'
+>>> 
+= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python37/deekshitha iit class.py
+no sol
+>>> 
