7d30c4fb6ebe73d39f1393769c950c498aef337c
PotnuruDeekshitha <139770330+PotnuruDeekshitha@users.noreply.github.com>
2023-07-29 16:57:54 +0530

    Add files via upload

---
+++ deekshitha coordinate geometry.py
@@ -0,0 +1,8 @@
+import numpy as np
+print("find the point of trisection of the line segment joining")
+A = np.array[4,-1]
+k = np.array[2,1]
+C = np.array[-2,-3]
+B = np.array[A+k@C]/[k+1]
+x = np.array[4+2@-2]/[2+1]
+y = np.array[-1+2@-3]/[2+1]   
